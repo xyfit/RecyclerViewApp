50a86c668fda442b97b62aa2ed6971b04d0b2f06
sardormobile <gooduzcom@gmail.com>
2022-10-23 11:37:57 -0400

    # item onClick

--- app/src/main/java/uz/uzmbapps/recyclerapp/adapters/HomeAdapter.kt
+++ app/src/main/java/uz/uzmbapps/recyclerapp/adapters/HomeAdapter.kt
@@ -1,11 +1,15 @@
 package uz.uzmbapps.recyclerapp.adapters
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import uz.uzmbapps.recyclerapp.ChatActivity
 import uz.uzmbapps.recyclerapp.R
 import uz.uzmbapps.recyclerapp.models.ItemModel
 
@@ -22,6 +26,16 @@ class HomeAdapter(val lists: List<ItemModel>):RecyclerView.Adapter<HomeAdapter.I
         val itemData = lists[position]
         holder.itemView.findViewById<TextView>(R.id.name_id).text = itemData.name
         holder.itemView.findViewById<TextView>(R.id.message_id).text = itemData.message
+
+//        holder.itemView.findViewById<ConstraintLayout>(R.id.item_id).setOnClickListener {
+//            Toast.makeText(holder.itemView.context, "itemView", Toast.LENGTH_SHORT).show()
+//        }
+        holder.itemView.setOnClickListener {
+            val i = Intent(holder.itemView.context, ChatActivity::class.java)
+            holder.itemView.context.startActivity(i)
+        }
+
+
     }
 
     override fun getItemCount(): Int = lists.size
